c8e58285826dfcdcc66b2d8f7db74e29c496e358
jwjwjiawen <58043400+jwjwjiawen@users.noreply.github.com>
2024-04-09 14:58:28 +0800

    Update settings.py
    修改了cookies和视频照片存储路径

--- weibo/settings.py
+++ weibo/settings.py
@@ -12,7 +12,7 @@ DEFAULT_REQUEST_HEADERS = {
     'Accept':
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7',
-    'cookie': 'your cookie'
+    'cookie': 'XSRF-TOKEN=yx-Ub6Ziqv-0aOeYHQBO4hYA; SUB=_2A25LEJMODeRhGeRH61YU9yrPzTmIHXVob6rGrDV8PUNbmtB-LVHbkW9NTf_9wi61avaLBIWclGAeQMBqWUVwnv7l; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9W5lP.fdZjK1AFp3WhsBIcKW5NHD95QE1K5XSKMXe0qfWs4Dqcj6i--Xi-zRiKyFi--Xi-zRi-8Wi--4iK.Ni-z7i--fi-z4i-zXi--fiKy2iKnpi--fiKnfi-8Fi--fi-zEiKnp; ALF=02_1715236958; WBPSESS=QnD5GsOtfPGKI98W4QOWBib774kmLTnXBpWpM4Hm-frftev6FhExHJMBF55WX60_Nhx_vi6HFHri2k5CZ19d2o-3Z3hzcMHLQ2ewXrvhGyDu_TvZfZOmcRl5wMUAVn7dgIvbchWCc8oYE13UiMu4RQ=='
 }
 ITEM_PIPELINES = {
     'weibo.pipelines.DuplicatesPipeline': 300,
@@ -24,25 +24,25 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['迪丽热巴']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['河南暴雨']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
-WEIBO_TYPE = 1
+WEIBO_TYPE = 0
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
 CONTAIN_TYPE = 0
 # 筛选微博的发布地区，精确到省或直辖市，值不应包含“省”或“市”等字，如想筛选北京市的微博请用“北京”而不是“北京市”，想要筛选安徽省的微博请用“安徽”而不是“安徽省”，可以写多个地区，
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2020-03-01'
+START_DATE = '2021-06-01'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2020-03-01'
+END_DATE = '2022-06-01'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
 # 图片文件存储路径
-IMAGES_STORE = './'
+IMAGES_STORE = '/Users/jx/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/A21/小论文/data/images'
 # 视频文件存储路径
-FILES_STORE = './'
+FILES_STORE = '/Users/jx/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/A21/小论文/data/videos'
 # 配置MongoDB数据库
 # MONGO_URI = 'localhost'
 # 配置MySQL数据库，以下为默认配置，可以根据实际情况更改，程序会自动生成一个名为weibo的数据库，如果想换其它名字请更改MYSQL_DATABASE值
