71fdf1e08691e315270c0741560d3765891e4613
jwjwjiawen <58043400+jwjwjiawen@users.noreply.github.com>
2024-09-18 14:05:54 +0800

    Update settings.py

--- weibo/settings.py
+++ weibo/settings.py
@@ -24,7 +24,7 @@ ITEM_PIPELINES = {
 }
 # 要搜索的关键词列表，可写多个, 值可以是由关键词或话题组成的列表，也可以是包含关键词的txt文件路径，
 # 如'keyword_list.txt'，txt文件中每个关键词占一行
-KEYWORD_LIST = ['河南暴雨']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
+KEYWORD_LIST = ['中储粮']  # 或者 KEYWORD_LIST = 'keyword_list.txt'
 # 要搜索的微博类型，0代表搜索全部微博，1代表搜索全部原创微博，2代表热门微博，3代表关注人微博，4代表认证用户微博，5代表媒体微博，6代表观点微博
 WEIBO_TYPE = 0
 # 筛选结果微博中必需包含的内容，0代表不筛选，获取全部微博，1代表搜索包含图片的微博，2代表包含视频的微博，3代表包含音乐的微博，4代表包含短链接的微博
@@ -33,16 +33,16 @@ CONTAIN_TYPE = 0
 # 具体支持的地名见region.py文件，注意只支持省或直辖市的名字，省下面的市名及直辖市下面的区县名不支持，不筛选请用“全部”
 REGION = ['全部']
 # 搜索的起始日期，为yyyy-mm-dd形式，搜索结果包含该日期
-START_DATE = '2021-06-01'
+START_DATE = '2024-07-02'
 # 搜索的终止日期，为yyyy-mm-dd形式，搜索结果包含该日期
-END_DATE = '2022-06-01'
+END_DATE = '2024-08-31'
 # 进一步细分搜索的阈值，若结果页数大于等于该值，则认为结果没有完全展示，细分搜索条件重新搜索以获取更多微博。数值越大速度越快，也越有可能漏掉微博；数值越小速度越慢，获取的微博就越多。
 # 建议数值大小设置在40到50之间。
 FURTHER_THRESHOLD = 46
 # 图片文件存储路径
-IMAGES_STORE = '/Users/jx/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/A21/小论文/data/images'
+IMAGES_STORE = '/Users/jx/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/A21/小论文/Raw_Data/images'
 # 视频文件存储路径
-FILES_STORE = '/Users/jx/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/A21/小论文/data/videos'
+FILES_STORE = '/Users/jx/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/A21/小论文/Raw_Data/videos'
 # 配置MongoDB数据库
 # MONGO_URI = 'localhost'
 # 配置MySQL数据库，以下为默认配置，可以根据实际情况更改，程序会自动生成一个名为weibo的数据库，如果想换其它名字请更改MYSQL_DATABASE值
